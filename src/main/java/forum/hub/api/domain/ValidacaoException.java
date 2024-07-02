6a9e9a1eb5dbaaae8e5269a6e7f3c2663a2525ab
biancaBezerra <bianca.skills@gmail.com>
2024-07-02 17:55:38 -0300

    criando classe e migration usuario

---
+++ src/main/java/forum/hub/api/domain/ValidacaoException.java
@@ -0,0 +1,7 @@
+package forum.hub.api;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/main/java/forum/hub/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package forum.hub.api.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
